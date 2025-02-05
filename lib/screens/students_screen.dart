7614015b20239adac021b305272087a26e2585ed
Karaama143 <cabdicumar747@gmail.com>
2025-02-05 16:22:36 +0300

    my Fast Commit

---
+++ lib/screens/students_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:school_app/widgets/card_btn.dart';
+
+import 'screens.dart';
+import 'student_registration.dart';
+
+class StudentsScreen extends StatelessWidget {
+  const StudentsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          title: Text("Students"),
+          centerTitle: true,
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              child: CardBtn(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_)=>StudentRegistration()));
+                } ,
+                backgroundColor: Colors.brown.shade400,
+                title: "Register Student",
+                iconData: Icons.app_registration,
+                )
+                ,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              child: CardBtn(
+                onPressed: () {
+                   Navigator.push(context, MaterialPageRoute(builder: (_)=>StudentListScreen()));
+                } ,
+                backgroundColor: Colors.brown.shade400,
+                title: "All students",
+                iconData: Icons.read_more_sharp,
+                ),
+            ),
+            
+          ],
+        ));
+  }
+}
